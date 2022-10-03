5c8cc3bf03cd690065a6676b2813d4262f65b33a
DmArkanoid <68047962+DmArkanoid@users.noreply.github.com>
2022-10-03 00:54:47 -0400

    Header completed

--- 3d-store/src/Components/AccountIcon.js
+++ 3d-store/src/Components/AccountIcon.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AccountIcon.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 /*import NoAccountsIcon from '@mui/icons-material/NoAccounts'; */
 
 
-
-
 function AccountIcon() {
+  const [buttonState, setButtonState] = useState();
+  
+  
   return (
     <div className="account">
         
       <div className="accountIcon">
-        <AccountCircleIcon id="accountIcon" />
+        <AccountCircleIcon 
+          id="accountIcon" 
+          onClick = {() => setButtonState(!buttonState)} 
+        />
       </div>
 
-      {
-
+      { buttonState === true &&(
       <div className="account__options">
-        <ul>
-          <li><a href="#">Profile</a></li>
-          <li><a href="#">Login</a></li>
-          <li><a href="#">Logout</a></li>
+        <ul> 
+          <li onClick={() => setButtonState(!buttonState)}><a href="www.google.com" target="_blank">Profile</a></li>
+          <li onClick={() => setButtonState(!buttonState)}><a href="www.google.com" target="_blank">Login</a></li>
+          <li onClick={() => setButtonState(!buttonState)}><a href="www.google.com" target="_blank">Logout</a></li>
         </ul>
       </div> 
-      
-      }   
+      )}
+    
     </div>
   )
 }
